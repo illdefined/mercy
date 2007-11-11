865a8f9a8ab71556c2d96e72b080afb6266cd7d4
Maximilian Gass <mxey@arcor.de>
2007-11-11 22:20:18 +0100

    Fixed braindeadism in function naming, build before commit please!

--- seek_col.c
+++ seek_col.c
@@ -1,6 +1,6 @@
 #include "mercy.h"
 
-int mercy_seek(struct mercy_context *ctx, unsigned short col) {
+int mercy_seek_col(struct mercy_context *ctx, unsigned short col) {
 	if (buffer_puts(&ctx->buf, _MERCY_ESCAPE) ||
 		buffer_putulong(&ctx->buf, col + 1) ||
 		buffer_put(&ctx->buf, "G", 1))
